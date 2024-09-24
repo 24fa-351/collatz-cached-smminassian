226789b0a476b82717260aba301fc0771ac9dade
smminassian <sebastienminassian203@gmail.com>
2024-09-24 13:49:01 -0700

    I made some progress but I dont know if my code truely works and the reason why is because my debugger is screwed up

--- collatz.c
+++ collatz.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int collatz(int result)
+{
+    int N;
+    int Max;
+    int Min;
+    int RN;
+    int steps = 0;
+
+    scanf("%d %d %d", N, Min, Max);
+    for (int ix = 0; ix <= N; ix++)
+    {
+        RN = rand() % (Max - Min);
+        if (RN % 2 == 0)
+        {
+            result = RN / 2;
+            steps++;
+        }
+        else if (RN % 2 != 0)
+        {
+            result = (3 * RN) + 1;
+            steps++;
+        }
+        if (result != 1)
+        {
+            collatz(result);
+        }
+        else if (result == 1)
+        {
+            break;
+        }
+    }
+    return RN, steps;
+}
+
+int main()
+{
+    int collatzResult = 0;
+    int RN = 0;
+    int steps;
+
+    RN = collatz(collatzResult);
+    steps = collatz(collatzResult);
     
+
 }
