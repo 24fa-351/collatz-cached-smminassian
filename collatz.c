ba951c3a90e048669ce498869d88c8ab5a83c6da
smminassian <sebastienminassian203@gmail.com>
2024-09-27 17:04:09 -0700

    So the idea is this
    I created a struct and this struct holds N and value. Ill have to change the N to RN but ill do it after my break.
    
    So starting in main, I first call collatz_wrapper. What the wrapper does is it create a cache to store the values that I get from collatz and then generates N random numbers and and calculates the amount of steps it takes to do collatz. Then it takes those steps, it takes the current RN, and the cache, and its size and it sends it to input. Wait i just realized something. The reason why my program is returning nothing is because I am saying cacheSize  < 20 but cacheSize is 20. So I will say cacheIndex < cacheSize instead and i will inc cache Index. But anyways, I convert RN to char and I set the the char N to to key which is RN. Then I do the same for value but this time i set steps to it. Then I set its key and steps to their corresponding values then I return cache. Ill explain it after my break. Pce

--- collatz.c
+++ collatz.c
@@ -1,53 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int collatz(int result, int N, int Min, int Max, int steps)
+typedef struct
 {
-    result = 0;
+    char N;
+    int Value;
+} items;
+
+int cacheIndex = 0;
+items *input(int RN, int steps, items *cache, int cacheSize)
+{
+   
+    if (cacheIndex < cacheSize)
+    {
+        char key = (char)RN;
+        cache[cacheIndex].N = key;
+        cache[cacheIndex].Value = steps;
+        cacheIndex++;
+    }
+    return cache;
+}
+
+int collatz(int RN)
+{
+    int steps = 0;
+    int oneChecker;
+    oneChecker = RN;
+
+    while (RN != 1)
+    {
+
+        if (RN % 2 == 0)
+        {
+            RN = RN / 2;
+            steps++;
+        }
+        else if (RN % 2 != 0)
+        {
+            RN = (3 * RN) + 1;
+            steps++;
+        }
+        else if (oneChecker == 1)
+        {
+            steps++;
+            break;
+        }
+    }
+    return steps;
+}
+
+items *collatz_wrapper(int N, int Min, int Max, int cacheSize)
+{
+  
+    items *cache = (items *)malloc(sizeof(items) * cacheSize);
+    int steps = 0;
     int RN = 0;
 
     for (int ix = 0; ix <= N; ix++)
     {
         RN = rand() % (Max - Min);
-        if (RN % 2 == 0)
-        {
-            result = RN / 2;
-            steps++;
-        }
-        else if (RN % 2 != 0)
-        {
-            result = (3 * RN) + 1;
-            steps++;
-        }
-        if (result != 1)
-        {
-            collatz(result, N, Min, Max, steps);
-        }
-        else if (result == 1)
-        {
-            ix++;
-        }
+
+        steps = collatz(RN);
+
+        cache = input(RN, steps, cache, cacheSize);
+        
     }
 
-    return RN, steps;
+    return cache;
+    
 }
 
 int main(int __argc, char *__argv[])
 {
-    int collatzResult = 0;
-    int steps = 0;
+
+    int cacheSize = 20;
     int N = 0;
     int Min = 0;
     int Max = 0;
-    int result = 0;
-    int RN = 0;
-if(__argc == 4){
-    N = atoi(__argv[1]);
-    Min = atoi(__argv[2]);
-    Max = atoi(__argv[3]);
-    RN, steps = collatz(result, N, Min, Max, steps);
 
-    printf("%d", RN);
-    printf("%d", steps);
-}
+    items *cache = (items *)malloc(sizeof(items) * cacheSize);
+    if (__argc == 4)
+    {
+        N = atoi(__argv[1]);
+        Min = atoi(__argv[2]);
+        Max = atoi(__argv[3]);
+
+        
+    
+    cache = collatz_wrapper(N, Min, Max, cacheSize);
+    for(int i = 0; i < cacheSize; i++){
+        printf("N: %d  Steps: %d ", cache[i].N, cache[i].Value);
+    }
+    }
+    free(cache);
+    return 0;
 }
