6f2785ab72017bb294ad55b5829e8bce28e9a54a
smminassian <sebastienminassian203@gmail.com>
2024-09-28 20:17:27 -0700

    debugged my program on online gdb.
    Why u may ask? Its because my debugger on vs code dosent accept command line arguments and its a hassle setting it up.
    
    So what Im trying to do right now is set up MRU. However, Im trying to change the tenth index but its just not working. I need to take a break ive been working for 2 hours straight.

--- collatz.c
+++ collatz.c
@@ -1,26 +1,157 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// typedef struct
+// {
+//     char N;
+//     int Value;
+//     int Count;
+// } items;
+
+// int cacheIndex = 0;
+// int count = 0;
+// items *input(int RN, int steps, items *cache, int cacheSize)
+// {
+//     if (cacheIndex < cacheSize)
+//     {
+//         char key = (char)RN;
+//         cache[cacheIndex].N = key;
+//         cache[cacheIndex].Value = steps;
+//         cacheIndex++;
+//         count++;
+//         cache[cacheIndex].Count = count;
+//     }
+//     else if(cacheIndex > cacheSize){
+//         cache = LRU(cacheSize, cache, count, cacheIndex);
+//     }
+//     return cache;
+// }
+
+// items *LRU(int cacheSize, items* cache, int count, int cacheIndex){
+ 
+    
+// }
+
+// int collatz(int RN)
+// {
+//     int steps = 0;
+//     int oneChecker;
+//     oneChecker = RN;
+
+//     while (RN != 1)
+//     {
+
+//         if (RN % 2 == 0)
+//         {
+//             RN = RN / 2;
+//             steps++;
+//         }
+//         else if (RN % 2 != 0)
+//         {
+//             RN = (3 * RN) + 1;
+//             steps++;
+//         }
+//         else if (oneChecker == 1)
+//         {
+//             steps++;
+//             break;
+//         }
+//     }
+//     return steps;
+// }
+
+// items *collatz_wrapper(int N, int Min, int Max, int cacheSize)
+// {
+  
+//     items *cache = (items *)malloc(sizeof(items) * cacheSize);
+//     int steps = 0;
+//     int RN = 0;
+
+//     for (int ix = 0; ix < N; ix++)
+//     {
+//         RN = rand() % (Max - Min) + Min;
+
+//         steps = collatz(RN);
+
+//         cache = input(RN, steps, cache, cacheSize);
+        
+//     }
+
+//     return cache;
+    
+// }
+
+// int main(int __argc, char *__argv[])
+// {
+
+//     int cacheSize = 20;
+//     int N = 0;
+//     int Min = 0;
+//     int Max = 0;
+
+//     items *cache = (items *)malloc(sizeof(items) * cacheSize);
+//     if (__argc == 4)
+//     {
+//         N = atoi(__argv[1]);
+//         Min = atoi(__argv[2]);
+//         Max = atoi(__argv[3]);
+
+//     }
+    
+//     cache = collatz_wrapper(N, Min, Max, cacheSize);
+//     for(int i = 0; i < cacheSize; i++){
+//         printf("%d , %d\n ", cache[i].N, cache[i].Value, cache[i].Count);
+//     }
+    
+//     free(cache);
+//     return 0;
+// }
+
+
+//I will implement that time counter that he said in lecture. Moreover look at 44:03 in lecture. 
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct
 {
-    char N;
-    int Value;
+    char rN;
+    int steps;
     int Count;
 } items;
 
 int cacheIndex = 0;
-items *input(int RN, int steps, items *cache, int cacheSize)
-{
-   
-    if (cacheIndex < cacheSize)
-    {
-        char key = (char)RN;
-        cache[cacheIndex].N = key;
-        cache[cacheIndex].Value = steps;
-        cacheIndex++;
-    }
+int count = 0;
+int lruCount = 0;
+
+items* LRU(char key, int steps, items* cache){
+    
+    cache[lruCount].rN = key;
+    cache[lruCount].steps = steps;
+    cache[lruCount].Count = lruCount;
+    lruCount++;
+    
     return cache;
 }
+
+items *input(int RN, int steps, items *cache, int cacheSize)
+{
+    char key;
+    if (cacheIndex < cacheSize)
+    {
+        key = (char)RN;
+        cache[cacheIndex].rN = key;
+        cache[cacheIndex].steps = steps;
+        cacheIndex++;
+        count++;
+        cache[cacheIndex].Count = count;
+    }
+    else if(cacheIndex > cacheSize){
+        cache = LRU(key, steps, cache);
+    }
+    
+    return cache;
+}
+
 
 int collatz(int RN)
 {
@@ -57,7 +188,7 @@ items *collatz_wrapper(int N, int Min, int Max, int cacheSize)
     int steps = 0;
     int RN = 0;
 
-    for (int ix = 0; ix <= N; ix++)
+    for (int ix = 0; ix < N; ix++)
     {
         RN = rand() % (Max - Min) + Min;
 
@@ -74,7 +205,7 @@ items *collatz_wrapper(int N, int Min, int Max, int cacheSize)
 int main(int __argc, char *__argv[])
 {
 
-    int cacheSize = 20;
+    int cacheSize = 11;
     int N = 0;
     int Min = 0;
     int Max = 0;
@@ -90,7 +221,7 @@ int main(int __argc, char *__argv[])
     
     cache = collatz_wrapper(N, Min, Max, cacheSize);
     for(int i = 0; i < cacheSize; i++){
-        printf("%d , %d\n ", cache[i].N, cache[i].Value);
+        printf("%d , %d, %d\n ", cache[i].rN, cache[i].steps, cache[i].Count);
     }
     
     free(cache);
